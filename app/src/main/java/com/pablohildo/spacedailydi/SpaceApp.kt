2bd9f292e7aed33d746404ce7c95beadf6fd4340
Pablo <pablohildo@gmail.com>
2020-03-10 21:31:03 -0300

    Configures custom App class

--- app/src/main/java/com/pablohildo/spacedailydi/SpaceApp.kt
+++ app/src/main/java/com/pablohildo/spacedailydi/SpaceApp.kt
@@ -1,7 +1,23 @@
 package com.pablohildo.spacedailydi
 
 import android.app.Application
+import com.pablohildo.spacedailydi.di.appModule
+import com.pablohildo.spacedailydi.di.networkModule
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class SpaceApp : Application() {
+    val defaultCurrentActivityListener: DefaultCurrentActivityListener by inject()
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@SpaceApp)
+            modules(listOf(appModule, networkModule))
+        }
 
+        registerActivityLifecycleCallbacks(defaultCurrentActivityListener)
+    }
 }
