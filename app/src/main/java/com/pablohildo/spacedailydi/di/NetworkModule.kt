4423a003c7225e0bdb06339a461e3231daf4939e
Pablo <pablohildo@gmail.com>
2020-03-10 21:10:49 -0300

    Creates Retrofit instance

--- app/src/main/java/com/pablohildo/spacedailydi/di/NetworkModule.kt
+++ app/src/main/java/com/pablohildo/spacedailydi/di/NetworkModule.kt
@@ -2,19 +2,23 @@ package com.pablohildo.spacedailydi.di
 
 import com.pablohildo.spacedailydi.BuildConfig
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
 val networkModule = module {
-    single(named("BASE_URL")) { "https://api.nasa.gov/planetary/apod" }
+    single(named("BASE_URL")) { "https://api.nasa.gov/planetary/" }
     single {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
         interceptor
     }
+
     single {
         val client = OkHttpClient()
             .newBuilder()
@@ -26,5 +30,20 @@ val networkModule = module {
         }
         client.build()
     }
-    single { Moshi.Builder().add(KotlinJ) }
+
+    single {
+        Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    single {
+        Retrofit
+            .Builder()
+            .baseUrl(get<String>(named("BASE_URL")))
+            .addConverterFactory(MoshiConverterFactory.create(get()))
+            .client(get())
+            .build()
+    }
 }
